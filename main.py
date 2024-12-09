870ec7e6f1ce8eb791722e312cda290a7df9a3fe
FernandoJRR <fercitotron@gmail.com>
2024-12-09 13:11:24 -0600

    feat: add retry insert batches

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import os
 from utils.downloader import download_csv_from_url
-from source.database import obtener_conexion
+from source.database import crear_tablas, insertar_datos, drop_registros
 
 url_casos_globales = "https://drive.google.com/uc?export=download&id=1V-vFNkkYiSPQClASZeKx-cTvwlE5tOFD"
 path_casos_locales = os.path.join(os.path.dirname(__file__), 'data', 'municipio.csv')
 anio = 2020
+
+def validar_tipos_columnas(df, tipos):
+    for columna, tipo in tipos.items():
+        if columna in df.columns:
+            df = df[df[columna].apply(lambda x: isinstance(x, tipo))]
+    return df
 
 if __name__ == "__main__":
     #Se obtiene el archivo global
@@ -59,20 +65,39 @@ if __name__ == "__main__":
     #Se eliminan los duplicados
     dfLocal = dfLocal.drop_duplicates()
 
+    #Se eliminan los municipios no registrados
+    dfLocal = dfLocal[dfLocal['codigo_municipio'] != 0]
+
+    # Crear un diccionario con el primer ID encontrado para cada departamento
+    id_departamentos_unicos = dfLocal.groupby('departamento')['codigo_departamento'].first().to_dict()
+    # Actualizar el DataFrame con el ID correcto
+    dfLocal['codigo_departamento'] = dfLocal['departamento'].map(id_departamentos_unicos)
+
     # Reestructurar el datalocal para adaptarlo a las necesidades del modelo de datos
     dfLocal = dfLocal.melt(
         id_vars=['departamento', 'codigo_departamento', 'municipio', 'codigo_municipio', 'poblacion'],
         var_name='fecha', value_name='casos'
     )
 
+    #Las fechas XX se convierten a 20XX
+    dfLocal['fecha'] = dfLocal['fecha'].apply(lambda x: x if len(x.split('/')[-1]) == 4 else x[:-2] + '20' + x[-2:])
     dfLocal['fecha'] = pd.to_datetime(dfLocal['fecha'], format='%m/%d/%Y', errors='coerce')
     dfLocal['fallecidos'] = 0
 
     print("Unificando Dataframes")
     dfCombinado = pd.concat([dfLocal, dfGlobal], ignore_index=True)
+    dfCombinado['casos'] = pd.to_numeric(dfCombinado['casos'], errors='coerce').astype('Int64')
+    dfCombinado['fallecidos'] = pd.to_numeric(dfCombinado['fallecidos'], errors='coerce').astype('Int64')
+    dfCombinado = dfCombinado[dfCombinado['casos'].notnull()]
+    dfCombinado = dfCombinado[dfCombinado['fallecidos'].notnull()]
+    dfCombinado.dropna()
+    print(dfCombinado.info())
 
-
-    print(dfGlobal.head())
-    print(dfLocal.head(10))
-    print(dfCombinado.head(20))
-    print(dfCombinado.shape)
+    print(dfCombinado.tail(10))
+    print("Se prepara la base de datos")
+    print("Se crean las tablas si no existen")
+    crear_tablas()
+    print("Drop a todos los registros")
+    drop_registros()
+    print("Insertando registros")
+    insertar_datos(dfCombinado)
--- source/database.py
+++ source/database.py
@@ -1,11 +1,16 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date, Float
-from sqlalchemy.orm import declarative_base
+import pandas as pd
+import time
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date, Float, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 # Configuración de la base de datos
 DATABASE_URL = "postgresql+psycopg2://fernanrod@localhost/ss2_practica1_db"
 
 # Crear el motor de la base de datos
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
+
+# Crear sesionmaker
+Session = sessionmaker(bind=engine)
 
 # Declaración base para ORM
 Base = declarative_base()
@@ -13,7 +18,7 @@ Base = declarative_base()
 # Definir la tabla Departamento
 class Departamento(Base):
     __tablename__ = 'departamento'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     nombre = Column(String(255), nullable=False, unique=True)
 
     # Relación con la tabla Municipio
@@ -22,8 +27,9 @@ class Departamento(Base):
 # Definir la tabla Municipio
 class Municipio(Base):
     __tablename__ = 'municipio'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     nombre = Column(String(255), nullable=False)
+    poblacion = Column(Integer, nullable=False)
     id_departamento = Column(Integer, ForeignKey('departamento.id'), nullable=False)
 
     # Relación con tablas
@@ -37,7 +43,7 @@ class Registro(Base):
     fecha = Column(Date, nullable=False)
     casos = Column(Integer, nullable=False)
     fallecidos = Column(Integer, nullable=False)
-    id_municipio = Column(Integer, ForeignKey('municipio.id'), nullable=False)
+    id_municipio = Column(Integer, ForeignKey('municipio.id'), nullable=True)
 
     # Relación con la tabla Municipio
     municipio = relationship("Municipio", back_populates="registros")
@@ -48,6 +54,113 @@ def crear_tablas():
     Base.metadata.create_all(engine)
     print("Tablas creadas correctamente.")
 
-# Función para obtener la conexión
-def obtener_conexion():
-    return engine.connect()
+def drop_registros():
+    session = Session()
+    try:
+        print("Eliminando todos los registros...")
+
+        # Borrar registros en orden de dependencias
+        session.query(Registro).delete()
+        session.query(Municipio).delete()
+        session.query(Departamento).delete()
+
+        # Confirmar cambios
+        session.commit()
+        print("Todos los registros han sido eliminados correctamente.")
+
+    except Exception as e:
+        # Revertir cambios si ocurre un error
+        session.rollback()
+        print(f"Error al eliminar los registros: {e}")
+
+    finally:
+        session.close()
+
+# Insertar datos en la base de datos
+def insertar_datos(df):
+    session = Session()
+    lotes_fallidos = []
+    qty_lotes_exitosos = 0
+    qty_lotes_fallidos = 0
+    try:
+        print("INSERTANDO DEPARTAMENTOS")
+        # Crear diccionarios para bulk insert
+        dfDepartamentos = df[df['codigo_departamento'].notna() & (df['departamento'] != '0')]
+        departamentos = dfDepartamentos[['codigo_departamento', 'departamento']].drop_duplicates().rename(
+            columns={'codigo_departamento': 'id', 'departamento': 'nombre'}
+        ).to_dict('records')
+        print(departamentos)
+
+        # Insertar departamentos
+        session.bulk_insert_mappings(Departamento, departamentos)
+        session.commit()
+        print("DEPARTAMENTOS INSERTADOS")
+
+        print("INSERTANDO MUNICIPIOS")
+        # Crear lista de municipios
+        dfMunicipios = df[df['codigo_municipio'].notna() & (df['municipio'] != '0')]
+        municipios = dfMunicipios[['codigo_municipio', 'municipio', 'codigo_departamento', 'poblacion']].drop_duplicates().rename(
+            columns={'codigo_municipio': 'id', 'municipio': 'nombre', 'codigo_departamento': 'id_departamento'}
+        ).to_dict('records')
+        print(municipios)
+
+        # Insertar municipios
+        session.bulk_insert_mappings(Municipio, municipios)
+        session.commit()
+        print("MUNICIPIOS INSERTADOS")
+
+        # Crear lista de registros
+        registros = df[['fecha', 'casos', 'fallecidos', 'codigo_municipio']].rename(
+            columns={'codigo_municipio': 'id_municipio'}
+        )
+
+        # Reemplazar None con NULL explícito
+        registros['id_municipio'] = registros['id_municipio'].where(pd.notnull(registros['id_municipio']), None)
+        registros = registros.to_dict('records')
+
+        # Insertar registros
+        lotes = (len(registros) + 49) // 50
+        for i, inicio in enumerate(range(0, len(registros), 50)):
+            lote = registros[inicio:inicio+50]
+
+            try:
+                session.bulk_insert_mappings(Registro, lote)
+                session.commit()
+                qty_lotes_exitosos += 1
+                print(f"INSERTADO LOTE #{i+1}/#{lotes} DE {len(lote)} REGISTROS")
+            except Exception as e:
+                session.rollback()
+                lotes_fallidos.append((i+1, lote))
+                qty_lotes_fallidos += 1
+                print(f"FALLIDO LOTE #{i+1}/#{lotes} DE {len(lote)} REGISTROS")
+
+        print("Carga de Datos Inicial Finalizada")
+        print(f"{qty_lotes_exitosos}/{lotes} Insertados Correctamente")
+        if qty_lotes_fallidos > 0:
+            print(f"Hay {qty_lotes_fallidos} registros que no fueron insertados")
+            print("Esperando 10 segundos antes de reintentar")
+            qty_lotes_fallidos = 0
+            time.sleep(10)
+
+            qty_lotes_reinsertados = 0
+            for i, batch in lotes_fallidos:
+                try:
+                    print(f"REINTENTANDO LOTE #{i} DE {len(batch)} REGISTROS")
+                    session.bulk_insert_mappings(Registro, batch)
+                    session.commit()
+                    print(f"INSERTADO LOTE #{i} DE {len(batch)} REGISTROS")
+                except Exception as e:
+                    session.rollback()
+                    print(f"FALLA FINAL DE LOTE #{i} DE {len(lote)} REGISTROS")
+                    print(f"ERROR: {e}")
+
+            print(f"{qty_lotes_reinsertados} LOTES FALLIDOS REINSERTADOS")
+        else:
+            print("TODOS LOS REGISTROS INSERTADOS CORRECTAMENTE")
+
+    except Exception as e:
+        session.rollback()
+        print(f"Error al insertar los datos: {e}")
+
+    finally:
+        session.close()
